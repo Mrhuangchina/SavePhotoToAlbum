4b92e28eab2426b7a8577b442d75ec1a8c542aa8
Mrhuangchina <jianmissyoulove@qq.com>
2017-07-02 22:26:43 +0800

    Initial Commit

---
+++ SavePhotoToAlbum/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SavePhotoToAlbum
+//
+//  Created by MrHuang on 17/7/2.
+//  Copyright © 2017年 Mrhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SavePhotoToAlbum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SavePhotoToAlbum
+//
+//  Created by MrHuang on 17/7/2.
+//  Copyright © 2017年 Mrhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
